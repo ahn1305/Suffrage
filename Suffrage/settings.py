efc55794aecb1c04946499c056a8d5b90b7ef056
ahn1305 <ahnashwin1305@gmail.com>
2021-10-29 21:09:58 +0530

    removed the aws-ses email backend and changed it to gmail backend

--- Suffrage/settings.py
+++ Suffrage/settings.py
@@ -153,16 +153,5 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'ab7710850@gmail.com'
 EMAIL_HOST_PASSWORD = 'ilmdcekcywupteyn'
 
-# EMAIL_BACKEND = 'django_ses.SESBackend'
-# DEFAULT_FROM_EMAIL = 'ahnashwin1305@gmail.com'
-
-
-# AWS_ACCESS_KEY_ID = 'AKIA6ALP4WRQ3VA5HDOG'
-# AWS_SECRET_ACCESS_KEY = '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'
-# AWS_SES_REGION_NAME = 'us-east-2'
-# AWS_SES_REGION_ENDPOINT = 'email.us-east-2.amazonaws.com'
-
-#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
 
 AUTH_USER_MODEL = 'users.CustomUser' # new
